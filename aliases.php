4c3bfd069c0704f38715b6208d8b22fa4fcc8376
Henrique Moody <henriquemoody@gmail.com>
2026-02-09 13:41:42 +0100

    Fix REUSE compliance in aliases.php
    Update SPDX headers to reuse lint requirements by consolidating
    license and contributor information.

--- aliases.php
+++ aliases.php
@@ -1,13 +1,9 @@
 <?php
 
 /*
- * SPDX-License-Identifier: MIT
  * SPDX-FileCopyrightText: (c) Respect Project Contributors
- * SPDX-FileContributor: Alexandre Gomes Gaigalas <alganet@gmail.com>
- * SPDX-FileContributor: Andy Wendt <andy@awendt.com>
- * SPDX-FileContributor: Graham Campbell <graham@mineuk.com>
+ * SPDX-License-Identifier: ISC
  * SPDX-FileContributor: Henrique Moody <henriquemoody@gmail.com>
- * SPDX-FileContributor: Nick Lombard <github@jigsoft.co.za>
  */
 
 declare(strict_types=1);
